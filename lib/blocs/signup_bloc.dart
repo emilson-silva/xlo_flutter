1cbc481000ba55497b97d6c61c0000f8c4d3bbca
Emilson Silva <emilson.silva@tgcore.com.br>
2020-06-08 16:37:19 -0300

    create SignUpBloc

--- lib/blocs/signup_bloc.dart
+++ lib/blocs/signup_bloc.dart
@@ -2,6 +2,11 @@ import 'package:rxdart/rxdart.dart';
 
 enum SignUpState { IDLE, LOADING, ERROR }
 
+class SignUpBloState {
+  SignUpState state;
+  String errorMessage;
+}
+
 class SignUpBloc {
   final BehaviorSubject _stateController = BehaviorSubject();
 
