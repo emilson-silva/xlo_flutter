742e107d13d620635cc98e60a59e34b43f4613b3
Emilson Silva <emilson.silva@tgcore.com.br>
2020-09-08 15:43:32 -0300

    create product tile

--- lib/screens/product/widgets/user_panel.dart
+++ lib/screens/product/widgets/user_panel.dart
@@ -8,6 +8,46 @@ class UserPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.stretch,children: <Widget>[Padding(padding: EdgeInsets.only(top: ),)],);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 18.0, bottom: 18.0),
+          child: Text(
+            'Anunciante',
+            style: TextStyle(
+              fontSize: 17.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(8.0),
+          color: Colors.grey[200],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Emilson',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Na OLX desde hoje',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
